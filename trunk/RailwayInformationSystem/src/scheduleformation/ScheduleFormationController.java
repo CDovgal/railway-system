e2afa302e157d11a567471fbb8c92e136a9f83f7
c.dovgal <c.dovgal@407a2ec8-8607-410a-aff7-896afa79928d>
2013-02-20 23:30:09 +0000

    add interface to ScheduleFormation form

--- trunk/RailwayInformationSystem/src/scheduleformation/ScheduleFormationController.java
+++ trunk/RailwayInformationSystem/src/scheduleformation/ScheduleFormationController.java
@@ -4,11 +4,25 @@
  */
 package scheduleformation;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 import javafx.scene.control.Menu;
 import javafx.scene.control.Tab;
 import javafx.scene.text.Font;
@@ -18,6 +32,7 @@ import javafx.scene.text.Font;
  *
  * @author Oleksander
  */
+
 public class ScheduleFormationController implements Initializable {
     @FXML
     private Menu sf_file;
@@ -33,10 +48,19 @@ public class ScheduleFormationController implements Initializable {
     /**
      * Initializes the controller class.
      */
+
+    @FXML
+    private Button CloseButton;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    private void CloseButton(ActionEvent event) throws IOException {
+        System.exit(0);
+    }
 
     @FXML
     private void sf_trains_tab_selection(Event event) {
